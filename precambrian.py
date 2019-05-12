dd1ae0d89879711f1646fefa6534c799232fdb26
ilic-stefan <ilicstef@grinnell.edu>
2019-05-12 15:44:40 -0500

    fixed initialization issue and corrected mean square error loss implementation! We should be back to the original results promised.

--- precambrian.py
+++ precambrian.py
@@ -34,11 +34,8 @@ IMWidth = math.floor(IMWidth)
 # resize content image to a height by width by 3 image
 content_img = content_img[IMHeight:IMHeight+height, IMWidth:IMWidth+width, :]
 
-# show the smaller content image with openCV
-# cv2.imshow("image", content_img)
-# cv2.waitKey(0)
-#
-# print(content_img.shape)
+# convert to doubles from 0 to 1
+content_img = np.divide(content_img, 255.0)
 
 # Build Computation Graph #
 
@@ -53,10 +50,16 @@ cimg = tf.placeholder(tf.float32, (height, width, 3))
 # construct developing image tensor (variable)
 with tf.variable_scope("core"):
     dimg = tf.get_variable("developing-img", [height, width, 3],
-                           dtype=tf.float32, initializer=tf.random_normal_initializer)
+                           dtype=tf.float32, initializer=tf.random_uniform_initializer(0, 1))
+
+    # $$$ testing out backpropagation...
+    #dimg2 = tf.get_variable("test-variable-to-be-deleted", [height, width, 3],
+                           #dtype=tf.float32, initializer=tf.random_uniform_initializer(0, 1))
+
+    #tf.assign(dimg2, dimg)
 
     # use a mean square error between dimg and cimg for loss function
-    loss = tf.reduce_sum(tf.subtract(dimg, cimg)) ** 2 / (height*width)
+    loss = tf.reduce_sum(tf.subtract(dimg, cimg) ** 2) / (height*width)
 
 # specify optimizer
 opt = tf.train.AdamOptimizer()
@@ -75,7 +78,7 @@ with tf.Session() as sess:
     # keep track of the loss values for every iteration
     loss_history_lst = [] # make a list to hold all loss_val tensors
 
-    iteration_count = 10000 # the number of gradient descent steps we will take
+    iteration_count = 4000 # the number of gradient descent steps we will take
 
     for _ in range(iteration_count):
         # do a gradient descent step
@@ -87,14 +90,27 @@ with tf.Session() as sess:
     # evaluate loss_history stack
     out_loss_history = sess.run(loss_history_stack)
     # evaluate developing image to get output image
-    outimg = sess.run(dimg)
+    outImg = sess.run(dimg)
+
+    print("evaluation done.")
+    print("preparing results report...")
+
+    # Get max and min statistics about outimg
+    # note: should be between 0 and 1
+    print("outImg max:")
+    print(sess.run(tf.reduce_max(outImg)))
+    print("outImg min:")
+    print(sess.run(tf.reduce_min(outImg)))
 
 # plot the loss history
-# iteration_history = np.arange(iteration_count)
-# plt.scatter(iteration_history, out_loss_history)
-# plt.title("loss during optimization")
-# plt.show()
+iteration_history = np.arange(iteration_count)
+plt.scatter(iteration_history, out_loss_history)
+plt.title("loss during optimization")
+plt.show()
 
 # Display Resulting Image #
-cv2.imshow("reconstructed image", outimg)
+cv2.imshow("original image", content_img)
+cv2.imshow("reconstructed image", outImg)
 cv2.waitKey(0)
+
+print("results done.")
